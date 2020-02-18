109e896d9f645e8f90f147dea6a4dcc0998e5fa7
Jonathan McCormick <jonathan@ssmj.co.uk>
2020-02-18 16:41:46 +0000

    added instance in 2 and 3 plus classic  access

--- main.tf
+++ main.tf
@@ -9,19 +9,24 @@ locals {
      ZONE3  = "${var.region3}-1"
    }
 
-
+#Create VPC
 resource "ibm_is_vpc" "vpc1" {
   provider = "ibm.us"
   name = "vpc-${var.region1}-${random_id.name1.hex}"
+  classic_access = "true"
 }
 resource "ibm_is_vpc" "vpc2" {
   provider = "ibm.de"
   name = "vpc-${var.region2}-${random_id.name1.hex}"
+  classic_access = "true"
 }
 resource "ibm_is_vpc" "vpc3" {
   provider = "ibm.tok"
   name = "vpc-${var.region3}-${random_id.name1.hex}"
+  classic_access = "true"
 }
+
+#Create Single subnet per VPC
 resource "ibm_is_subnet" "subnet1" {
   provider = "ibm.us"
   name            = "subnet-${var.region1}-${random_id.name1.hex}"
@@ -63,6 +68,7 @@ resource "ibm_is_ssh_key" "sshkeyus" {
   provider   = "ibm.us"
   name       = "${var.ssh_key_name}-${random_id.name1.hex}"
   public_key = "${var.ssh_public_key}"
+  
 }
 resource "ibm_is_ssh_key" "sshkeyde" {
   provider   = "ibm.de"
@@ -77,7 +83,7 @@ resource "ibm_is_ssh_key" "sshkeytok" {
 
 resource "ibm_is_instance" "instance1" {
   provider = "ibm.us"
-  name    = "instance-${random_id.name1.hex}"
+  name    = "instance-us-${random_id.name1.hex}"
   image   = "${var.image}"
   profile = "${var.profile}"
 
@@ -92,11 +98,11 @@ resource "ibm_is_instance" "instance1" {
 
 resource "ibm_is_floating_ip" "floatingip1" {
   provider = "ibm.us"
-  name   = "fip-${random_id.name1.hex}"
+  name   = "fip-us-${random_id.name1.hex}"
   target = "${ibm_is_instance.instance1.primary_network_interface.0.id}"
 }
 
-resource "ibm_is_security_group_rule" "sg1_tcp_rule" {
+resource "ibm_is_security_group_rule" "us_sg1_tcp_rule" {
   provider   = "ibm.us"
   depends_on = ["ibm_is_floating_ip.floatingip1"]
   group      = "${ibm_is_vpc.vpc1.default_security_group}"
@@ -109,7 +115,7 @@ resource "ibm_is_security_group_rule" "sg1_tcp_rule" {
   }
 }
 
-resource "ibm_is_security_group_rule" "sg1_icmp_rule" {
+resource "ibm_is_security_group_rule" "us_sg1_icmp_rule" {
   provider   = "ibm.us"
   depends_on = ["ibm_is_floating_ip.floatingip1"]
   group      = "${ibm_is_vpc.vpc1.default_security_group}"
@@ -122,7 +128,7 @@ resource "ibm_is_security_group_rule" "sg1_icmp_rule" {
   }
 }
 
-resource "ibm_is_security_group_rule" "sg1_app_tcp_rule" {
+resource "ibm_is_security_group_rule" "us_sg1_app_tcp_rule" {
   provider   = "ibm.us"
   depends_on = ["ibm_is_floating_ip.floatingip1"]
   group      = "${ibm_is_vpc.vpc1.default_security_group}"
@@ -135,6 +141,122 @@ resource "ibm_is_security_group_rule" "sg1_app_tcp_rule" {
   }
 }
 
-output "zone1subnet1" {
-  value = "${ibm_is_subnet.subnet1.ipv4_cidr_block}"
+resource "ibm_is_instance" "instance2" {
+  provider = "ibm.de"
+  name    = "instance-de-${random_id.name1.hex}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet     = "${ibm_is_subnet.subnet2.id}"
+  }
+
+  vpc       = "${ibm_is_vpc.vpc2.id}"
+  zone      = "${local.ZONE2}"
+  keys      = ["${ibm_is_ssh_key.sshkeyde.id}"]
+}
+
+resource "ibm_is_floating_ip" "floatingip2" {
+  provider = "ibm.de"
+  name   = "fip-de-${random_id.name1.hex}"
+  target = "${ibm_is_instance.instance2.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_security_group_rule" "de_sg1_tcp_rule" {
+  provider   = "ibm.de"
+  depends_on = ["ibm_is_floating_ip.floatingip2"]
+  group      = "${ibm_is_vpc.vpc2.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "de_sg1_icmp_rule" {
+  provider   = "ibm.de"
+  depends_on = ["ibm_is_floating_ip.floatingip2"]
+  group      = "${ibm_is_vpc.vpc2.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  icmp = {
+    code = 0
+    type = 8
+  }
+}
+
+resource "ibm_is_security_group_rule" "de_sg1_app_tcp_rule" {
+  provider   = "ibm.de"
+  depends_on = ["ibm_is_floating_ip.floatingip2"]
+  group      = "${ibm_is_vpc.vpc2.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
+}
+
+resource "ibm_is_instance" "instance3" {
+  provider = "ibm.tok"
+  name    = "instance-tok-${random_id.name1.hex}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet     = "${ibm_is_subnet.subnet3.id}"
+  }
+
+  vpc       = "${ibm_is_vpc.vpc3.id}"
+  zone      = "${local.ZONE3}"
+  keys      = ["${ibm_is_ssh_key.sshkeytok.id}"]
+}
+
+resource "ibm_is_floating_ip" "floatingip3" {
+  provider = "ibm.tok"
+  name   = "fip-tok-${random_id.name1.hex}"
+  target = "${ibm_is_instance.instance3.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_security_group_rule" "tok_sg1_tcp_rule" {
+  provider   = "ibm.tok"
+  depends_on = ["ibm_is_floating_ip.floatingip3"]
+  group      = "${ibm_is_vpc.vpc3.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "tok_sg1_icmp_rule" {
+  provider   = "ibm.tok"
+  depends_on = ["ibm_is_floating_ip.floatingip3"]
+  group      = "${ibm_is_vpc.vpc3.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  icmp = {
+    code = 0
+    type = 8
+  }
+}
+
+resource "ibm_is_security_group_rule" "tok_sg1_app_tcp_rule" {
+  provider   = "ibm.tok"
+  depends_on = ["ibm_is_floating_ip.floatingip3"]
+  group      = "${ibm_is_vpc.vpc3.default_security_group}"
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
 }
